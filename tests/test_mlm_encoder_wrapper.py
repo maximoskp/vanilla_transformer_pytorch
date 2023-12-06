0f02beeebd52b3bc2c0ff9483a278bf269e416c8
maximoskp <maximoskalpap@gmail.com>
2023-12-06 12:45:06 +0200

    cleaned up test mlm

--- tests/test_mlm_encoder_wrapper.py
+++ tests/test_mlm_encoder_wrapper.py
@@ -16,30 +16,11 @@ num_layers = 6
 d_ff = 2048
 max_seq_length = 100
 dropout = 0.1
-batch_size = 2
-
-from x_transformers import TransformerWrapper, Encoder
-transformer = TransformerWrapper(
-    num_tokens = src_vocab_size,
-    max_seq_len = max_seq_length,
-    attn_layers = Encoder(
-        dim = d_model,
-        depth = num_layers,
-        heads = num_heads
-    )
-)
-print(transformer.parameters())
-# not working, out of memory
-# summary(transformer.cuda(), (batch_size, max_seq_length, src_vocab_size))
-
-# not working with encoderModel:
+batch_size = 16
 
 encoderModel = EncoderModel(src_vocab_size, d_model, num_heads, num_layers, d_ff, max_seq_length, dropout)
 mlmEncoderWrapper = MLMEncoderWrapper(encoderModel)
 print(encoderModel.parameters())
-# not working:
-# RuntimeError: Expected tensor for argument #1 'indices' to have one of the following scalar types: Long, Int; but got torch.cuda.FloatTensor instead (while checking arguments for embedding)
-# summary(encoderModel.cuda(), (batch_size, max_seq_length, src_vocab_size))
 
 trainer = MLM(
     mlmEncoderWrapper,
@@ -51,11 +32,9 @@ trainer = MLM(
 ).cuda()
 
 # optimizer
-
 opt = Adam(trainer.parameters(), lr=3e-4)
 
 # one training step (do this for many steps in a for loop, getting new `data` each time)
-
 data = randint(0, src_vocab_size, (batch_size, max_seq_length)).cuda()
 output = mlmEncoderWrapper(data)
 print(output.shape)
